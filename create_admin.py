dbd2e0765a04dc4fe7f7d2d02657d78213697e36
GayathriNairMars <emp2mayalabs@gmail.com>
2026-05-13 17:26:23 +0530

    added create_admin

--- create_admin.py
+++ create_admin.py
@@ -1,10 +1,19 @@
 import os
 import django
+
+# 1. Setup Django environment
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ivf.settings')
 django.setup()
-from django.contrib.auth.models import User
-if not User.objects.filter(username='admin').exists():
-    User.objects.create_superuser('admin', 'admin@gmail.com', 'admin123')
-    print("Superuser created successfully!")
+
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+email = 'admin@gmail.com'
+password = 'admin123'
+
+if not User.objects.filter(email=email).exists():
+     User.objects.create_superuser(email=email,password=password)
+     print(f"Superuser {email} created successfully!")
 else:
-    print("Superuser already exists.")
+  print(f"User with email {email} already exists.")
