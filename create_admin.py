7e251cebb078969d6513541f1c4adc1078a6ef78
GayathriNairMars <emp2mayalabs@gmail.com>
2026-05-14 11:41:06 +0530

    new superuser admin created

--- create_admin.py
+++ create_admin.py
@@ -9,8 +9,8 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-email = 'admin@gmail.com'
-password = 'admin123'
+email = 'shravan@gmail.com'
+password = 'shravan123'
 
 if not User.objects.filter(email=email).exists():
      User.objects.create_superuser(email=email,password=password)
