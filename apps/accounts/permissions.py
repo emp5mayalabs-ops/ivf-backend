46de36c86367cc2d3f007d0b9fa85965a4088293
GayathriNairMars <emp2mayalabs@gmail.com>
2026-05-14 14:32:39 +0530

    changed permissions

--- apps/accounts/permissions.py
+++ apps/accounts/permissions.py
@@ -3,6 +3,9 @@ from rest_framework.permissions import BasePermission
 class StaffPermission(BasePermission):
     def has_permission(self, request, view):
         if not request.user.is_authenticated:
+            return False 
+        # These actions are allowed for ALL authenticated users
+        if view.action in ["heartbeat", "my_profile", "force_password_change"]:
             return True
         #ADMIN role permissions
         if request.user.is_staff:
@@ -10,6 +13,5 @@ class StaffPermission(BasePermission):
         #HR Manager role permissions
         if view.action in ["list","create","onboard","dashboard","my_profile",'hr_permissions','toggle_status','rec_permissions','audit-log']:
             return request.user.role == "HRM"
-        if view.action in [""]:
-            return 
+        
         return False
